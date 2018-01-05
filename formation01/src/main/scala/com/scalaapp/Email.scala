a1702ae12b05d73e9ef71cd593f915a04975460d
Antoine PERBAL <aperbal@solocal.com>
2018-01-05 17:50:06 +0100

    010002

---
+++ formation01/src/main/scala/com/scalaapp/Email.scala
@@ -0,0 +1,9 @@
+package com.scalaapp
+
+/**
+  * Created by aperbal on 05/01/2018.
+  */
+
+case class Email(val address: String) {
+
+}
---
+++ formation01/src/main/scala/com/scalaapp/ScalaApp.scala
@@ -0,0 +1,10 @@
+package com.scalaapp
+
+/**
+  * Created by aperbal on 05/01/2018.
+  */
+object ScalaApp {
+  def main(args: Array[String]): Unit = {
+    print("Hello, World !")
+  }
+}
